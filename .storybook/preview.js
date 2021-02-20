0e5f10f5e23617abe83848a50db390a4e74cb10e
sleduardo20 <sl.eduardo20@gmail.com>
2021-02-20 13:29:43 -0300

    (chore)🛠: config global styles in storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,24 @@
+import {GlobalStyles} from '../src/styles/GlobalStyles';
+
+export const decorators = [
+  (Story) => (
+    <>
+      <GlobalStyles/>
+      <Story />
+    </>
+  ),
+];
+
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
+  backgrounds:{
+    default: 'bg01',
+    values:[
+      {
+        name:'bg01',
+        value: '#DBE2EF',
+      }
+    ]
+  }
 }

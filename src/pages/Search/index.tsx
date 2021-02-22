b78e237f9e1e918696408f6e9ac453c09d91b511
sleduardo20 <sl.eduardo20@gmail.com>
2021-02-22 12:25:06 -0300

    (feat)🧠: create page Search

---
+++ src/pages/Search/index.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+import Base from '../../components/Base';
+import Button from '../../components/Button';
+import Card, { CardProps } from '../../components/Card';
+import Input from '../../components/Input';
+
+import * as S from './styles';
+
+const mockCard: CardProps = {
+  name: 'Brazil',
+  capital: 'Brasília',
+  flag: 'https://restcountries.eu/data/bra.svg',
+};
+
+const Search = () => {
+  return (
+    <Base>
+      <S.Content>
+        <h1>Countries Information</h1>
+
+        <form>
+          <Input name="search" placeholder="Search country" />
+          <Button title="search">Search</Button>
+        </form>
+
+        <S.GridCards>
+          <Card {...mockCard} />
+          <Card {...mockCard} />
+          <Card {...mockCard} />
+          <Card {...mockCard} />
+          <Card {...mockCard} />
+          <Card {...mockCard} />
+          <Card {...mockCard} />
+          <Card {...mockCard} />
+          <Card {...mockCard} />
+          <Card {...mockCard} />
+          <Card {...mockCard} />
+          <Card {...mockCard} />
+        </S.GridCards>
+      </S.Content>
+    </Base>
+  );
+};
+
+export default Search;

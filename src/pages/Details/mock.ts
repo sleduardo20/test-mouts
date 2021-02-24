b0e268ca6536146a830aa4f603fcd5077f84d45c
sleduardo20 <sl.eduardo20@gmail.com>
2021-02-24 12:05:37 -0300

    (feat)🧠: create data mock  page Details

---
+++ src/pages/Details/mock.ts
@@ -0,0 +1,60 @@
+export default {
+  Country: [
+    {
+      name: 'Brazil',
+      capital: 'Brasília',
+      area: 8515767,
+      population: 206135893,
+      officialLanguages: [
+        {
+          name: 'Portuguese',
+        },
+      ],
+      flag: {
+        svgFile: 'https://restcountries.eu/data/bra.svg',
+      },
+
+      topLevelDomains: [
+        {
+          name: '.br',
+        },
+      ],
+
+      distanceToOtherCountries: [
+        {
+          distanceInKm: 1333.0445603821204,
+          countryName: 'Bolivia (Plurinational State of)',
+        },
+        {
+          distanceInKm: 1481.9677422904354,
+          countryName: 'Paraguay',
+        },
+
+        {
+          distanceInKm: 2301.8016573888485,
+          countryName: 'Peru',
+        },
+        {
+          distanceInKm: 2345.5558225890036,
+          countryName: 'Venezuela (Bolivarian Republic of)',
+        },
+        {
+          distanceInKm: 2445.873412546106,
+          countryName: 'Colombia',
+        },
+        {
+          distanceInKm: 2562.4021099800075,
+          countryName: 'Uruguay',
+        },
+        {
+          distanceInKm: 2777.759849599381,
+          countryName: 'Chile',
+        },
+        {
+          distanceInKm: 2825.1600997734076,
+          countryName: 'Argentina',
+        },
+      ],
+    },
+  ],
+};
